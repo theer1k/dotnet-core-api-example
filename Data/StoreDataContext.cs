f847b8d153eba806623a84ce0d8f2f9a957f4f6d
Erik Henrique <erik.hac@outlook.com>
2019-05-19 22:34:46 -0300

    Mapping relational objects

--- Data/StoreDataContext.cs
+++ Data/StoreDataContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using ProductCatalog.Data.Maps;
 using ProductCatalog.Models;
 
 namespace ProductCatalog.Data {
@@ -8,6 +9,11 @@ namespace ProductCatalog.Data {
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             optionsBuilder.UseSqlServer(@"Server=localhost,1433;Database=prodcat;User ID=SA;Password=1q2w3e%&!");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            builder.ApplyConfiguration(new ProductMap());
+            builder.ApplyConfiguration(new CategoryMap());
         }
     }
 }
