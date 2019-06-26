75bccccc72ef42e7568d2c5c76dfe40cf9715b3b
Erik <erik.hac@outlook.com>
2019-06-26 00:09:58 -0300

    Add Category Controller

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using ProductCatalog.Data;
 
 namespace ProductCatalog
 {
@@ -12,6 +13,7 @@ namespace ProductCatalog
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddScoped<StoreDataContext, StoreDataContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
