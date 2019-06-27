f0bb705718448b99d28f19055927eaf7eccf091b
Erik <erik.hac@outlook.com>
2019-06-26 23:59:38 -0300

    Add ProductRepository and validations

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using ProductCatalog.Data;
+using ProductCatalog.Repositories;
 
 namespace ProductCatalog
 {
@@ -14,6 +15,7 @@ namespace ProductCatalog
         {
             services.AddMvc();
             services.AddScoped<StoreDataContext, StoreDataContext>();
+            services.AddTransient<ProductRepository, ProductRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
